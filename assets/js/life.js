824b96d225c8f4b8f88f6826fd4fa31e09ae413b
proko.chou <proko.chou@cloudmosa.com>
2018-10-08 22:32:41 +0100

    Change time

--- assets/js/life.js
+++ assets/js/life.js
@@ -3,7 +3,8 @@
 //calculate dates
 var total_days = 401;
 var today=new Date();
-var last_day=new Date(2019, 9, 14);
+
+var last_day=new Date(2019, 8, 14);
 var one_day=1000*60*60*24;
 var remaining=Math.ceil((last_day.getTime()-today.getTime())/(one_day));
 if (remaining >= 401){
@@ -15,6 +16,6 @@ var past = (100 - percentage).toFixed(1);
 
 
 window.onload = function what(){
-	document.getElementById("remain-id").innerHTML= "&nbsp;&nbsp;"+ remaining;
+	document.getElementById("remain-id").innerHTML= "&nbsp;"+ remaining;
 	document.getElementById("percentage").innerHTML= past + "%";
 };
